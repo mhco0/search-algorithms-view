308f33a923d6e3255565803724d95cd9b43947a2
Bezaliel Silva <bezaliel.silva@incognia.com>
2022-03-08 23:25:00 -0300

    feat(Interface): choose option using mouse click

--- run/Interface.py
+++ run/Interface.py
@@ -24,17 +24,28 @@ class Interface:
         textSize(self.text_size)
         
         fill(self.text_color)
-        prev_word_width = 0
+        prev_word_width = self.x
         
         for i in range(len(self.options)):
-            text("(" + self.keyboard_keys[i] + ")", self.x + prev_word_width + textWidth(self.options[i]) / 2 - textWidth("(" + self.keyboard_keys[i] + ")") / 2, self.y + self.text_size)
-            text(self.options[i], self.x + prev_word_width, self.y + 2 * self.text_size)
+            keyboard_key_text = "(" + self.keyboard_keys[i] + ")"
+            text(keyboard_key_text, prev_word_width + ((textWidth(self.options[i]) - textWidth(keyboard_key_text)) // 2), self.y + self.text_size)
+            text(self.options[i], prev_word_width, self.y + 2 * self.text_size)
             prev_word_width += textWidth(self.options[i]) + self.text_padding
      
     def binded_option(self, ikey):
         for i in range(len(self.keyboard_keys)):
             if self.keyboard_keys[i] == ikey:
                 return self.options[i]
+        return ""
+    
+    def clicked_option(self, pos):
+        prev_word_width = self.x
+        for i in range(len(self.options)):
+            x0 = prev_word_width
+            x1 = prev_word_width + textWidth(self.options[i])
+            if (pos[0] >= x0 and pos[0] <= x1 and pos[1] >= self.y and pos[1] <= self.y + 2 * self.text_size):
+                return self.options[i]
+            prev_word_width = x1 + self.text_padding
         return ""
     
     def keys_binded(self):
